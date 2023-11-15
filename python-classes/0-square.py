e57b281f120bba204fcf504350c6a44184326cbc
samkit4q <a.aderoba@alustudent.com>
2023-11-15 12:06:37 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 class square:
     pass
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+my_square = square()
+print(type(square))
+print(my_square._dict_)
