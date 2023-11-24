f203660fb9229732347abc631617c5370324ff98
samkit4q <a.aderoba@alustudent.com>
2023-11-23 23:58:47 -0800

    101-nqueens.py

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+"""Define a class nqueens N."""
+
+
+class  nqueens N:
+    """Represent a nqueens N."""
+
+print Usage: nqueens N
+1
+If N is not an integer,
+print N must be a number
