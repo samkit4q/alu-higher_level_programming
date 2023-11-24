b147113e59f84989bd181a3c794a2ff9b79768f8
samkit4q <a.aderoba@alustudent.com>
2023-11-24 04:15:33 -0800

    script updated

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+!/usr/bin/python3
+"""
+    Returns a list of available attributes and methods of an object.
+    """
+
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    """
+    attrs = dir(obj)
+    filtered_attrs = []
+    for attr in attrs:
+        if attr.startswith(''):
+            filtered_attrs.append(attr)
+
+    return filtered_attrs
