b6836e13751e91aeb88c58b0a9674d56bc395776
samkit4q <a.aderoba@alustudent.com>
2023-11-24 06:15:04 -0800

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,56 +1,23 @@
 #!/usr/bin/python3
-"""Module defining the Rectangle class."""
-
+"""Class Rectangle that inherits from BaseGeometry."""
 class BaseGeometry:
     """BaseGeometry class."""
-
     def integer_validator(self, name, value):
         """Validate that the value is a positive integer."""
         if type(value) is not int or value <= 0:
             raise ValueError(f"{name} must be a positive integer")
 
 class Rectangle(BaseGeometry):
-    """Rectangle class, inherits from BaseGeometry."""
-
+    """Represent a rectangle using BaseGeometry."""
     def __init__(self, width, height):
         """Initialize a new Rectangle.
 
         Args:
-            width (int): The width of the Rectangle.
-            height (int): The height of the Rectangle.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
-        self.__width = 0  # Initialize to 0, will be set by integer_validator
-        self.__height = 0  # Initialize to 0, will be set by integer_validator
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
-# Documentation for the module
-"""
-Module defining the Rectangle class.
-"""
-
-# Documentation for the BaseGeometry class
-"""
-BaseGeometry class.
-"""
-
-# Documentation for the Rectangle class
-"""
-Rectangle class, inherits from BaseGeometry.
-"""
-
-# Documentation for the integer_validator method
-"""
-Validate that the value is a positive integer.
-
-Args:
-    name (str): The name of the value being validated.
-    value (int): The value to be validated.
-
-Raises:
-    ValueError: If the value is not a positive integer.
-"""
-
-# Note: Do not include 'import' or 'from' inside comments as per the instructions.
