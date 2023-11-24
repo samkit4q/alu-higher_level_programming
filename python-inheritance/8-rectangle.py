4b73268ff8ad18563ba53c77ec5834fcddb0a85c
samkit4q <a.aderoba@alustudent.com>
2023-11-24 05:59:43 -0800

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    def integer_validator(self, name, value):
-        """Validate that the value is a positive integer."""
-        if type(value) is not int or value <= 0:
-            raise ValueError(f"{name} must be a positive integer")
-
-# 9-rectangle.py
-from 7-base_geometry import BaseGeometry
-
-class Rectangle(BaseGeometry):
+class Rectangle:
     def __init__(self, width, height):
         # Validate width and height as positive integers
         self.integer_validator("width", width)
@@ -17,3 +8,12 @@ class Rectangle(BaseGeometry):
         # Set private attributes
         self.__width = width
         self.__height = height
+
+    def integer_validator(self, name, value):
+        """Validate that the value is a positive integer."""
+        if type(value) is not int or value <= 0:
+            raise ValueError(f"{name} must be a positive integer")
+
+# Example usage:
+# my_rectangle = Rectangle(5, 10)
+
